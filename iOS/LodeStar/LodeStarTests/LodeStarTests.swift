04cec118e31c4359640cfc6cee0ec3b237a25107
celikkoseoglu <celikkoseoglu@yahoo.com>
2017-12-11 16:04:48 +0300

    iOS app template has been added

---
+++ iOS/LodeStar/LodeStarTests/LodeStarTests.swift
@@ -0,0 +1,36 @@
+//
+//  LodeStarTests.swift
+//  LodeStarTests
+//
+//  Created by Çelik Köseoğlu on 11.12.2017.
+//  Copyright © 2017 CrackerJack. All rights reserved.
+//
+
+import XCTest
+@testable import LodeStar
+
+class LodeStarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
