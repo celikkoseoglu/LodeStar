ac3b4733417343bdf2b01e64f3d413263a4d2eb4
Çelik Köseoğlu <celikkoseoglu@Celiks-MacBook-Pro.local>
2018-01-11 14:48:24 +0300

    Weather API merged with flight API

--- Server/LodeStarFlightStatus/server.js
+++ Server/LodeStarFlightStatus/server.js
@@ -1,11 +1,54 @@
 const express = require('express');
+const http = require('http');
 const app = express();
+
+function kelvinTo(what, temp) {
+    if (what === 0)
+        return temp - 273.15;
+    return temp * (9 / 5) - 459.67;
+}
 
 app.get('/', (req, res) => {
 
     if(req.query.dataType == "weather") {
 
-        res.send('your requested weather info for city: ' + req.query.city);
+        const APIKey = "c00ba395bae7607aaa0cf79dd388bb2d";
+
+        let request = http.request("http://api.openweathermap.org/data/2.5/forecast?q=" + req.query.city + "&appid=" + APIKey, function(response) {
+
+        console.log("Weather information for: " + req.query.city);
+
+        let replyMessage = "";
+        let jsonMessage  = "";
+
+        response.on('data', function(data) {
+            replyMessage = replyMessage + data;
+        });
+
+        response.on('end', function() {
+            jsonMessage = JSON.parse(replyMessage);
+
+            res.send(JSON.stringify(jsonMessage["list"][0], null, 4));
+
+            //console.log(jsonMessage["list"][1]);
+
+            let date1 = jsonMessage["list"][0].dt;
+
+            let temp1C = kelvinTo(0, jsonMessage["list"][0].main["temp"]);
+            let temp1K = kelvinTo(1, jsonMessage["list"][0].main["temp"]);
+
+            //console.log(days[moment.unix(date1).day() - 1] + " " + parseFloat(temp1C).toFixed(2) + "C , " + parseFloat(temp1K).toFixed(2) + "F");
+        });
+
+        //console.log("" + ${jsonMessage.list.dt});
+
+        });
+
+        request.end();
+
+        //res.send('you requested weather info for city: ' + req.query.city);
+
+
     }
 
     else if (req.query.dataType == "flightInfo") {
@@ -60,7 +103,6 @@ app.get('/', (req, res) => {
 
     else if (req.query.dataType == "nearbyVenues") {
         var coordinates = "40.7243,-74.0018"; //is to be replaced with req.query.coordinates
-
     }
 });
 
