d9aa4b0f45f70d6b43b1490a02c5acac84040e87
barispoyraz <brspoyraz@gmail.com>
2018-01-13 17:36:56 +0300

    Weather information activity and layout updated

---
+++ Android/LodeStar/app/src/main/java/com/lodestarapp/cs491/lodestar/WeatherInformationActivity.java
@@ -0,0 +1,30 @@
+package com.lodestarapp.cs491.lodestar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class WeatherInformationActivity extends Activity {
+
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_weather_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //Adapter
+
+    }
+
+}
