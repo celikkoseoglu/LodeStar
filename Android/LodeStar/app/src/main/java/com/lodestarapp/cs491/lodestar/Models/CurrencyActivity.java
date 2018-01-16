fc3d15a157a2532a24210fd02af921a5421c6682
hbeyan <huseyinbeyan@gmail.com>
2018-01-16 21:25:25 +0000

    Android currency page start

---
+++ Android/LodeStar/app/src/main/java/com/lodestarapp/cs491/lodestar/Models/CurrencyActivity.java
@@ -0,0 +1,15 @@
+package com.lodestarapp.cs491.lodestar.Models;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lodestarapp.cs491.lodestar.R;
+
+public class CurrencyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currency);
+    }
+}
