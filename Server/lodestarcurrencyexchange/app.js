f14f50a415cdcd7dc3aec7626a2da5bf32eab746
Çelik Köseoğlu <celikkoseoglu@Celiks-MacBook-Pro.local>
2018-01-17 14:08:00 +0300

    slight changes on the website

--- Server/lodestarcurrencyexchange/app.js
+++ Server/lodestarcurrencyexchange/app.js
@@ -1,26 +1,40 @@
+const express = require('express');
+const app = express();
 
 
-
-var fx = require('money'),
-    oxr = require('open-exchange-rates');
+var fx = require('money');
+var oxr = require('open-exchange-rates');
 
 oxr.set({
     app_id: '3e6839f0fda04541810a265a41b75183'
 });
 
-oxr.latest(function(error) {
+app.get('/', (req, res) => {
 
-    if ( error ) {
-        console.log( 'ERROR loading data from Open Exchange Rates API! Error was:' )
-        console.log( error.toString() );
+    oxr.latest(function(error) {
 
-        return false;
-    }
-    console.log( 'USD to HKD: ' + oxr.rates['HKD'] );
+        if ( error ) {
+            console.log( 'ERROR loading data from Open Exchange Rates API! Error was:' + error.toString() );
+            res.send( 'ERROR loading data from Open Exchange Rates API! Error was:' + error.toString() );
+            return false;
+        }
 
-    fx.rates = oxr.rates;
-    fx.base = oxr.base;
-    var amount = fx(10).from('EUR').to('GBP').toFixed(6);
-    console.log( '10 EUR to GBP: ' + amount );
+        //res.send(oxr.rates);
 
+        console.log( 'USD to HKD: ' + oxr.rates['HKD'] );
+
+        fx.rates = oxr.rates;
+        fx.base = oxr.base;
+        var amount = fx(1).from('EUR').to('GBP').toFixed(6);
+        console.log( '10 EUR to GBP: ' + amount );
+
+
+        var result = "{ \"result\": " + fx(1).from(req.query.currency).to(req.query.foreignCurrency).toFixed(4) + " }";
+        res.send(result);
+
+    });
+   
 });
+
+app.listen(3001, () => console.log('Example app listening on port 3001!'));
+
