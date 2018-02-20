520d4842e8aec73c44c4412e1249ca19ecd0d244
Çelik Köseoğlu <celikkoseoglu@Celiks-MacBook-Pro.local>
2018-02-21 00:11:24 +0300

    currency exchange returns symbols

--- Server/LodeStarCurrencyExchange/currencyExchange.js
+++ Server/LodeStarCurrencyExchange/currencyExchange.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 const fx = require('money');
 const oxr = require('open-exchange-rates');
+var fs = require('fs');
 
 oxr.set({
     app_id: '3e6839f0fda04541810a265a41b75183'
+});
+
+
+var obj;
+fs.readFile('common-currency.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    obj = JSON.parse(data);
+    console.log(data);
 });
 
 app.get('/', (req, res) => {
@@ -35,6 +44,8 @@ app.get('/', (req, res) => {
             response.localOverForeign = parseFloat(fx(1).from(foreignCurrency).to(localCurrency).toFixed(precision));
             response.eurosOverForeign = parseFloat(fx(1).from(standardCurrecny1).to(foreignCurrency).toFixed(precision));
             response.usdOverForeign = parseFloat(fx(1).from(standardCurrency2).to(foreignCurrency).toFixed(precision));
+            response.localCurrencySymbol = obj[localCurrency]['symbol'];
+            response.foreignCurrencySymbol = obj[foreignCurrency]['symbol'];
 
             res.send(JSON.stringify(response));
 
