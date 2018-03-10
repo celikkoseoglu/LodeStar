7346a972ea8c40330301258ff3504386014814f4
efeulasakayseyitoglu <efeulasakayseyitoglu@gmail.com>
2018-03-10 12:38:10 +0300

    commented out some code in login activity

--- Android/LodeStar/app/src/main/java/com/lodestarapp/cs491/lodestar/LoginActivity.java
+++ Android/LodeStar/app/src/main/java/com/lodestarapp/cs491/lodestar/LoginActivity.java
@@ -33,6 +33,9 @@ import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.lodestarapp.cs491.lodestar.Adapters.User;
+
+import java.util.ArrayList;
 
 import static android.widget.Toast.LENGTH_LONG;
 
@@ -55,6 +58,8 @@ public class LoginActivity extends AppCompatActivity implements
     private Button userSearchButton;
     private CallbackManager callbackManager;
     private DatabaseReference mDatabase;
+    ArrayList<User> userList;
+
 
     // private TextView alreadySignedInQuestion;
 
@@ -63,6 +68,8 @@ public class LoginActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        userList = new ArrayList<User>();
 
         FacebookSdk.setApplicationId(getString(R.string.facebook_app_id));
         //initialize Facebook SDK
@@ -129,7 +136,7 @@ public class LoginActivity extends AppCompatActivity implements
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
-       // Log.w(TAG, "Kobe0 ");
+       retrieveDBValues();
     }
 
 
@@ -279,22 +286,23 @@ public class LoginActivity extends AppCompatActivity implements
     private void retrieveDBValues() {
 
 
-        mDatabase.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
-            @Override
-            public void onDataChange(com.google.firebase.database.DataSnapshot dataSnapshot) {
-                for (com.google.firebase.database.DataSnapshot child : dataSnapshot.getChildren()) {
-                    for (com.google.firebase.database.DataSnapshot child2 : child.getChildren()) {
-                        Log.i("aga", child2.child("e-mail").toString());
-                    }
-                }
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+//        mDatabase.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
+//            @Override
+//            public void onDataChange(com.google.firebase.database.DataSnapshot dataSnapshot) {
+//                for (com.google.firebase.database.DataSnapshot child : dataSnapshot.getChildren()) {
+//                    for (com.google.firebase.database.DataSnapshot child2 : child.getChildren()) {
+//                        userList.add(new User(child2.child("username").toString(),child2.child("e-mail").toString()));
+//                        Log.i("aga", child2.child("e-mail").getValue().toString());
+//                    }
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
     }
 
 
