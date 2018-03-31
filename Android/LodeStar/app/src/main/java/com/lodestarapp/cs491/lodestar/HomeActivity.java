4547da29031ad4d57165187acb071453d852b5c0
efeulasakayseyitoglu <efeulasakayseyitoglu@gmail.com>
2018-03-31 20:33:37 +0300

    manually enter destation activity started (Home)

--- Android/LodeStar/app/src/main/java/com/lodestarapp/cs491/lodestar/HomeActivity.java
+++ Android/LodeStar/app/src/main/java/com/lodestarapp/cs491/lodestar/HomeActivity.java
@@ -10,24 +10,42 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeActivity extends Fragment {
+
+    Button manualSearchButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_home, container, false);
+
+
+        //reference to: https://stackoverflow.com/questions/21192386/android-fragment-onclick-button-method & http://www.androidbie.com/2017/07/tutorial-how-to-move-from-activity-to.html
+
+        View myView = inflater.inflate(R.layout.activity_home, container, false);
+
+        manualSearchButton = (Button) myView.findViewById(R.id.button4);
+
+        manualSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ManualDestinationActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+        return myView;
     }
 
     public void readQRCode(View view){
         Intent intent = new Intent(getActivity(), QRCodeActivity.class);
         startActivity(intent);
     }
+
+
 
 }
