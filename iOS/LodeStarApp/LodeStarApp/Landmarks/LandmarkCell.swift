42f471aa22e0f47101639109b979e7af68808327
berkevren <berkabbasoglu@hotmail.com>
2018-04-08 19:28:03 +0300

    Landmarks Screen added

---
+++ iOS/LodeStarApp/LodeStarApp/Landmarks/LandmarkCell.swift
@@ -0,0 +1,9 @@
+//
+//  LandmarkCell.swift
+//  LodeStarApp
+//
+//  Created by Berk Abbasoglu on 8.04.2018.
+//  Copyright © 2018 Berk Abbasoglu. All rights reserved.
+//
+
+import Foundation
---
+++ iOS/LodeStarApp/LodeStarApp/Landmarks/LandmarksViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  LodeStarApp
+//
+//  Created by Berk Abbasoglu on 17.01.2018.
+//  Copyright © 2018 Berk Abbasoglu. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class LandmarksViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //blackLayer.layer.cornerRadius = 8
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
