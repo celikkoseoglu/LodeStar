fcde2e8d1ef12c1dd9a4423887b08f1522739b51
efeulasakayseyitoglu <efeulasakayseyitoglu@gmail.com>
2018-05-06 12:23:14 +0300

    Home activity updated

--- Android/LodeStar/app/src/main/java/com/lodestarapp/cs491/lodestar/HomeActivity.java
+++ Android/LodeStar/app/src/main/java/com/lodestarapp/cs491/lodestar/HomeActivity.java
@@ -1,10 +1,13 @@
 package com.lodestarapp.cs491.lodestar;
 
+import android.app.FragmentManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -14,7 +17,7 @@ import android.widget.Button;
 
 public class HomeActivity extends Fragment {
 
-    Button manualSearchButton;
+    Button manualSearchButton,historyButton,vRRedirectButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,20 @@ public class HomeActivity extends Fragment {
         View myView = inflater.inflate(R.layout.activity_home, container, false);
 
         manualSearchButton = (Button) myView.findViewById(R.id.button4);
+
+        vRRedirectButton = (Button) myView.findViewById(R.id.button6);
+
+        //Referece for redirecting a web page: https://stackoverflow.com/questions/2236413/how-to-redirect-to-particular-url-while-clicking-on-button-in-android
+
+        vRRedirectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse("https://vr.google.com/cardboard/"));
+                startActivity(viewIntent);
+            }
+        });
+
+
 
         manualSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
